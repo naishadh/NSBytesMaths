16d6a6876ffe989be8b1dd25bcdbacd10cf60ddd
naishadh <naishadh.shroff@gmail.com>
2016-01-31 12:24:33 -0500

    First Commit

---
+++ NSBytesMaths/NSBytesMaths.cpp
@@ -0,0 +1,16 @@
+// NSBytesMaths.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hellow world !!!";
+	getchar();
+	return 0;
+}
+
