035ea00c0c4bcd472de83cc42dce3d246a331e4f
naishadh <naishadh.shroff@gmail.com>
2016-01-31 14:55:35 -0500

    Commit from work computer

--- NSBytesMaths/NSBytesMaths.cpp
+++ NSBytesMaths/NSBytesMaths.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Hellow world !!! From home computer";
+	cout << "Hellow world !!! From home computer"<< endl;
+	cout << "Hi from work computer...";
 	getchar();
 	return 0;
 }
