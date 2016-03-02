669f563d96356286c0d4f3fc890c958aad28a29b
naishadh <naishadh.shroff@gmail.com>
2016-03-01 23:12:44 -0500

    Back to application exe

---
+++ BunnyWorldTest/MOCKBunny.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "MOCKBunny.h"
+
+
+MOCKBunny::MOCKBunny()
+{
+}
+
+
+MOCKBunny::~MOCKBunny()
+{
+}
---
+++ BunnyWorldTest/MOCKBunny.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../BunnyWorldApplication/Bunny.h"
+#include "./gmock.h"
+class MOCKBunny : Bunny
+{
+public:
+	MOCKBunny();
+	virtual ~MOCKBunny();
+};
+
